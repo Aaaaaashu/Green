f0b786e1b06e1a53fc0224bc1065555cb2ed0279
orangehat36 <orangehat.rb@gmail.com>
2014-05-05 18:31:01 +0800

    Rename filename

--- src/main/java/com/flower/dao/TopicDao.java
+++ src/main/java/com/flower/dao/TopicDao.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.flower.domain.Topic;
 
 @Repository
-public class TopocDao extends BaseDao<Topic> {
+public class TopicDao extends BaseDao<Topic> {
 	
 	private final String GET_BOARD_DIGEST_TOPICS = "from Topic t where t.boardId = ? and digest > 0 order by t.lastPost desc, digest desc";
 	private final String GET_PAGED_TOPICS = "from Topic where boardId = ? order by lastPost desc";
